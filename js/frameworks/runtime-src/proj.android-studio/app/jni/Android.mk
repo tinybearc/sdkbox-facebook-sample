f208472b42e31d1d8092fa61e7f9711319ae5c04
wanghuan <wanghuan1115@live.cn>
2015-11-26 17:26:57 +0800

    sdkbox update for js 1.4.1.7

--- js/frameworks/runtime-src/proj.android-studio/app/jni/Android.mk
+++ js/frameworks/runtime-src/proj.android-studio/app/jni/Android.mk
@@ -9,6 +9,7 @@ LOCAL_MODULE_FILENAME := libcocos2djs
 LOCAL_SRC_FILES := hellojavascript/main.cpp ../../../Classes/AppDelegate.cpp ../../../Classes/PluginFacebookJS.cpp ../../../Classes/PluginFacebookJS.hpp ../../../Classes/PluginFacebookJSHelper.cpp ../../../Classes/PluginFacebookJSHelper.h ../../../Classes/SDKBoxJSHelper.cpp ../../../Classes/SDKBoxJSHelper.h
 
 LOCAL_LDLIBS := -landroid -llog
+LOCAL_CPPFLAGS := -DSDKBOX_ENABLED
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
 LOCAL_WHOLE_STATIC_LIBRARIES := PluginFacebook sdkbox
 
